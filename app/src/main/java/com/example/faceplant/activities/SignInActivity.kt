8d01f246cd5d6b7d5a7fb3a8b3137a8db78fa9ab
Umid0vic <57035358+Umid0vic@users.noreply.github.com>
2021-02-25 18:55:25 +0100

    Graphics for SignIn and SignUp activities

--- app/src/main/java/com/example/faceplant/activities/SignInActivity.kt
+++ app/src/main/java/com/example/faceplant/activities/SignInActivity.kt
@@ -1,12 +1,23 @@
 package com.example.faceplant.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import com.example.faceplant.R
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val signUpText = findViewById<TextView>(R.id.sign_up_text) as TextView
+
+        signUpText.setOnClickListener{
+            startActivity(
+                Intent(this, SignUpActivity::class.java)
+            )
+        }
     }
 }
--- app/src/main/java/com/example/faceplant/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/faceplant/activities/SignUpActivity.kt
@@ -1,23 +1,18 @@
 package com.example.faceplant.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.faceplant.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val loginBtn = findViewById<Button>(R.id.start_page_sign_in)
-
-        loginBtn.setOnClickListener{
-            startActivity(
-                Intent(this, SignInActivity::class.java)
-            )
-        }
-
+        setContentView(R.layout.activity_sign_up)
     }
 }
