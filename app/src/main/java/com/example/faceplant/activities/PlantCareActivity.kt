568fe842e5fac496d1c0c179df410577a14ec610
Matilda <55895602+akma18rd@users.noreply.github.com>
2021-03-05 11:46:21 +0100

    added plantcareactivity
    added PlantcareActivity, implemented design in xml

---
+++ app/src/main/java/com/example/faceplant/activities/PlantCareActivity.kt
@@ -0,0 +1,12 @@
+package com.example.faceplant.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.faceplant.R
+
+class PlantCareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_care)
+    }
+}
