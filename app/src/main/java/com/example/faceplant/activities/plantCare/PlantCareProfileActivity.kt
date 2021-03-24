ace9f56467e7938cd679b659e00d8e51c8784d0a
Matilda <55895602+akma18rd@users.noreply.github.com>
2021-03-24 21:00:40 +0100

    deleted fragments

--- app/src/main/java/com/example/faceplant/activities/plantCare/PlantCareProfileActivity.kt
+++ app/src/main/java/com/example/faceplant/activities/plantCare/PlantCareProfileActivity.kt
@@ -10,5 +10,13 @@ class PlantCareProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plant_care_profile)
+
+        //decaling variables for all clickable components
+        val lightIcon = findViewById<ImageView>(R.id.plantcare_profile_lighticon)
+        val waterIcon = findViewById<ImageView>(R.id.plantcare_profile_watericon)
+        val nutritionIcon = findViewById<ImageView>(R.id.plantcare_profile_nutritionicon)
+        val generalInfoIcon = findViewById<ImageView>(R.id.plantcare_profile_genralinfo)
+
+
     }
 }
